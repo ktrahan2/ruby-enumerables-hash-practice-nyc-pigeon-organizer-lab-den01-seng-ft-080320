ecdd31e2319b8f80422c61d549af59f8deae77f3
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-23 20:12:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,8 @@ def nyc_pigeon_organizer(data)
     value.each do |inner_key, names|
       names.each do |name|
         if !final_array[name] 
-          final_array << name
+          final_array[name] = {}
+      
         binding.pry
       end
     end
